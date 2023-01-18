fdd6500f027a7f1c6d5477fe1425635ff027fe8e
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2023-01-18 10:50:35 -0800

    Get api from tmdbApi

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import apiConfig from './api/api_config';
+import tmdbApi, { MovieType, TvType } from './api/tmdbApi';
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      try {
+        const movies = await tmdbApi.getMoviesList(MovieType.popular, 3)
+        const tv = await tmdbApi.getTvList(TvType.popular, 2)
+        console.log(movies)
+        console.log(tv)
+      }
+      catch (error) {
+        console.log(error)
+      }
+    })()
+  }, [])
   return (
     <div>
       Hello World!
