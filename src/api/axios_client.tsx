a85e164ae40c49a2b4a4e7cc754aa81d9b73f179
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2023-01-22 08:25:21 -0800

    Change interceptors

--- src/api/axios_client.tsx
+++ src/api/axios_client.tsx
@@ -8,10 +8,9 @@ const axiosClient = axios.create({
     },
 });
 
-axiosClient.interceptors.request.use(async (config) => config);
 axiosClient.interceptors.response.use((response) => {
     if (response && response.data) {
-        return response.data;
+        return response;
     }
     return response;
 }, (error) => {
