8bedb5922bf36865b0bd19182d1990001a3fcf56
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2023-02-05 20:11:31 -0800

    Get data from Api

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,38 @@
-import { Header } from "../components";
+import { useEffect, useState } from "react";
+import tmdbApi, { Movie, MovieType, Tv, TvType } from "../api/tmdbApi";
+import { Header, Slider } from "../components";
 import { Div } from "../styles";
 
 function Home() {
 
+    const [moviesPopular, setMoviesPopular] = useState<Movie[]>();
+    const [moviesTopRated, setMoviesTopRated] = useState<Movie[]>();
+    const [tvPopular, setTvPopular] = useState<Tv[]>();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const responsePopular = await tmdbApi.getMoviesList(MovieType.popular, 1)
+                const responseTopRated = await tmdbApi.getMoviesList(MovieType.top_rated, 1)
+                const responseTV = await tmdbApi.getTvList(TvType.popular, 1)
+                setMoviesPopular(responsePopular.data.results.slice(1, 20))
+                setMoviesTopRated(responseTopRated.data.results.slice(1, 20))
+                setTvPopular(responseTV.data.results.slice(1, 20))
+            }
+            catch (error) {
+                console.log(error)
+            }
+        })()
+    }, [])
+
     return (
         <Div>
             <Header />
+            <Div width="98%" margin="0 auto">
+                <Slider movies={moviesPopular} title={"Trending Movies"} />
+                <Slider movies={moviesTopRated} title={"Top Rated Movies"} />
+                <Slider movies={tvPopular} title={"Trending TV"} />
+            </Div>
         </Div>
     );
 }
