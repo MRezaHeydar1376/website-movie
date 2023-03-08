48c1e7978f678c65ae218582d2611faaed8d11c4
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2023-03-08 05:17:44 -0800

    Add prop hover

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import apiConfig from "../api/api_config";
-import { Div, H3, Img } from "../styles";
+import { Youtube } from "../assets";
+import { Div, H3, H4, Img } from "../styles";
 import { Color } from "../variables";
 
 interface Props {
@@ -8,9 +10,20 @@ interface Props {
     width?: string;
     height?: string;
     fontSize?: string;
+    hover: boolean;
 }
 
-function Card({ image, name, width, height, fontSize }: Props) {
+function Card({ image, name, width, height, fontSize, hover }: Props) {
+
+    const [mouseIn, setMouseIn] = useState(false)
+
+    function mouseInImage() {
+        setMouseIn(true);
+    };
+    function mouseOutImage() {
+        setMouseIn(false);
+    };
+
     return (
         <Div
             width={width !== undefined ? width : "350px"}
@@ -19,15 +32,37 @@ function Card({ image, name, width, height, fontSize }: Props) {
             align="center"
             margin="10px"
         >
-            <Img
-                src={apiConfig.w500Image(image)}
-                width={width !== undefined ? width : "350px"}
-                height={height !== undefined ? height : "500px"}
-                borderRadius="10px"
-            />
-            <H3 color={Color.white} fontSize={{ xs: `${fontSize !== undefined ? fontSize : "60px"}` }}>
+            <Div position="relative">
+                <Img
+                    src={apiConfig.w500Image(image)}
+                    width={width !== undefined ? width : "350px"}
+                    height={height !== undefined ? height : "500px"}
+                    borderRadius="10px"
+                    onMouseEnter={mouseInImage}
+                    onMouseOut={mouseOutImage}
+                    filter={mouseIn && hover ? "10%" : "100%"}
+                />
+                {mouseIn && hover ? (
+                    <Img
+                        onMouseEnter={mouseInImage}
+                        src={Youtube}
+                        height="60px"
+                        position="absolute"
+                        top="40%"
+                        left="35%"
+                        cursor="pointer"
+                    />
+                ) : null}
+            </Div>
+            <H4
+                color={mouseIn && hover ? Color.red : Color.white}
+                fontSize={{ xs: `${fontSize !== undefined ? fontSize : "60px"}` }}
+                fontWeight={{ xs: "600" }} 
+                textAlign="center"
+                margin="20px 0"
+            >
                 {name}
-            </H3>
+            </H4>
         </Div>
     );
 }
