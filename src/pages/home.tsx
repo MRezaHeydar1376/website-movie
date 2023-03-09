5da75f2b37b9fd59e881155ee89b6576ef69618b
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2023-03-09 12:27:15 -0800

    Add row Top Rated TV

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,6 +8,7 @@ function Home() {
     const [moviesPopular, setMoviesPopular] = useState<Movie[]>();
     const [moviesTopRated, setMoviesTopRated] = useState<Movie[]>();
     const [tvPopular, setTvPopular] = useState<Tv[]>();
+    const [tvTopRated, setTvTopRated] = useState<Tv[]>();
 
     useEffect(() => {
         (async () => {
@@ -15,9 +16,11 @@ function Home() {
                 const responsePopular = await tmdbApi.getMoviesList(MovieType.popular, 1)
                 const responseTopRated = await tmdbApi.getMoviesList(MovieType.top_rated, 1)
                 const responseTV = await tmdbApi.getTvList(TvType.popular, 1)
+                const responseTVTopRated = await tmdbApi.getTvList(TvType.top_rated, 1)
                 setMoviesPopular(responsePopular.data.results.slice(1, 20).map(({ original_title, ...movie }) => ({ ...movie, original_name: original_title })))
                 setMoviesTopRated(responseTopRated.data.results.slice(1, 20).map(({ original_title, ...movie }) => ({ ...movie, original_name: original_title })))
                 setTvPopular(responseTV.data.results.slice(1, 20))
+                setTvTopRated(responseTVTopRated.data.results.slice(1, 20))
             }
             catch (error) {
                 console.log(error)
@@ -32,6 +35,7 @@ function Home() {
                 <Slider movies={moviesPopular} title={"Trending Movies"} />
                 <Slider movies={moviesTopRated} title={"Top Rated Movies"} />
                 <Slider movies={tvPopular} title={"Trending TV"} />
+                <Slider movies={tvTopRated} title={"Top Rated TV"} />
             </Div>
         </Div>
     );
